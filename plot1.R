e90678c625a1b436e95b2f8623f6c0c5acbf9079
JackZhang0224 <pengzhang1986@yahoo.ca>
2017-08-20 10:06:04 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,25 @@
+## libraries
+require(ggplot2)
+
+## set my work dir
+setwd("C:/Users/jackzhang/Coursera/Exploring Data/Project2/")
+
+
+## get data
+file = "summarySCC_PM25.rds"
+NEI <- readRDS(file)
+
+
+## aggregate total PM25 emission from all sources per year
+plotdata <- aggregate(NEI[c("Emissions")], list(year = NEI$year), sum)
+
+## create the plot
+png('plot1.png', width=480, height=480)
+
+## plot data
+plot(plotdata$year, plotdata$Emissions, type = "l", 
+     main = "Total PM2.5 Emission in the US 1999-2008",
+     xlab = "Year", ylab = "Emissions")
+
+## close device
+dev.off()
