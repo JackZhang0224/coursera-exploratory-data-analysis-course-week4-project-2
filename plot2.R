f2a8c4982ca73c8682ba930c1e39f343c8b4462f
JackZhang0224 <pengzhang1986@yahoo.ca>
2017-08-20 10:14:39 -0700

    Create plot2.R
    Create the second plot

---
+++ plot2.R
@@ -0,0 +1,26 @@
+## libraries
+require(ggplot2)
+
+## set my work dir
+setwd("C:/Users/jackzhang/Coursera/Exploring Data/Project2/")
+
+
+## get data
+file = "summarySCC_PM25.rds"
+NEI <- readRDS(file)
+
+
+## aggregate total PM25 emission from Baltimore per year
+baltimore <- subset(NEI, fips == "24510")
+plotdata <- aggregate(baltimore[c("Emissions")], list(year = baltimore$year), sum)
+
+## create plot
+png('plot2.png', width=480, height=480)
+
+## plot data
+plot(plotdata$year, plotdata$Emissions, type = "l", 
+     main = "Total PM2.5 Emission in Baltimore 1999-2008",
+     xlab = "Year", ylab = "Emissions")
+
+## close device
+dev.off()
